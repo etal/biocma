15a7fa823980b73b1fa699712aba94dfb661ee3f
Eric Talevich <eric.talevich@gmail.com>
2013-09-09 11:38:24 -0700

    Bumped version by a point to fix botched PyPI upload

--- setup.py
+++ setup.py
@@ -3,20 +3,21 @@
 """I/O and utilities for the Consensus Multiple Alignment (CMA) file format."""
 
 from glob import glob
+from os.path import join
 try:
     from setuptools import setup
-except:
+except ImportError:
     from distutils.core import setup
 
 setup(
     name='biocma',
-    version='0.2',
+    version='0.2.1',
     description=__doc__,
     author='Eric Talevich',
     author_email='eric.talevich@gmail.com',
     url='http://github.com/etal/biocma',
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python :: 2.7',
@@ -24,6 +25,6 @@ setup(
         'Topic :: Scientific/Engineering :: Bio-Informatics',
     ],
     packages=['biocma'],
-    #scripts=glob('scripts/*'),
+    #scripts=glob(join('scripts', '*')),
 )
 
